d78edf64462d6e81fe217895acd79d04b20161c7
Monbrey <monbrey@gmail.com>
2021-05-28 08:06:53 +1000

    refactor: move additional response methods

--- src/structures/CommandInteraction.js
+++ src/structures/CommandInteraction.js
@@ -113,6 +113,6 @@ class CommandInteraction extends Interaction {
   followUp() {}
 }
 
-InteractionResponses.applyToClass(CommandInteraction);
+InteractionResponses.applyToClass(CommandInteraction, ['deferUpdate', 'update']);
 
 module.exports = CommandInteraction;
